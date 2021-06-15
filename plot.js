cfe4317c94bfa5db7c2aff2aa8700a4ec59bd0db
madisonfisher <mbfishe2@ncsu.edu>
2021-06-14 21:41:58 -0400

    updated formatting on the gauge chart

--- plot.js
+++ plot.js
@@ -116,21 +116,20 @@ d3.json("samples.json").then((samples) => {
             mode: "gauge+number",
             value: meta_wfreq,
             gauge: {
-                axis: { range: [null, 9] },
-                bar: { color: "darkgreen" },
-                bgcolor: "white",
+                axis: { range: [null, 9], nticks: 10 },
+                bar: { color: "#204f38" },
                 borderwidth: 2,
                 bordercolor: "gray",
                 steps: [
-                    { range: [0, 1], color: "rgb(225, 245, 238)" },
-                    { range: [1, 2], color: "rgb(200, 245, 238)" },
-                    { range: [2, 3], color: "rgb(175, 245, 238)" },
-                    { range: [3, 4], color: "rgb(150, 245, 238)" },
-                    { range: [4, 5], color: "rgb(125, 245, 238)" },
-                    { range: [5, 6], color: "rgb(100, 245, 238)" },
-                    { range: [6, 7], color: "rgb(75, 245, 238)" },
-                    { range: [7, 8], color: "rgb(50, 245, 238)" },
-                    { range: [8, 9], color: "rgb(25, 245, 238)" },
+                    { range: [0, 1], color: "#d9fdff" },
+                    { range: [1, 2], color: "#c3e4e6" },
+                    { range: [2, 3], color: "#b4d2d4" },
+                    { range: [3, 4], color: "#a3bdbf" },
+                    { range: [4, 5], color: "#91a7a8" },
+                    { range: [5, 6], color: "#7d9091" },
+                    { range: [6, 7], color: "#6e7f80" },
+                    { range: [7, 8], color: "#5b7070" },
+                    { range: [8, 9], color: "#4e6161" },
                 ],
             }
         };
@@ -140,7 +139,10 @@ d3.json("samples.json").then((samples) => {
 
         //gauge layout
         var layout3 = {
-            title: "Belly Button Wash Frequency"
+            title: "Belly Button Wash Frequency",
+            width: 500, 
+            height: 425, 
+
         };
 
         //gauge plot
